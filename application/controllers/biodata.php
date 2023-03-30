374daf5c4911f298f2d10c95288f56334d3e47e0
Adham Zafi <adhamzafi91@gmail.com>
2023-03-30 17:40:29 +0700

    Tugas Pertemuan 2

---
+++ application/controllers/biodata.php
@@ -0,0 +1,15 @@
+<?php
+
+class biodata extends CI_Controller {
+
+
+	public function biodataku()
+	{
+		$this->load->view('biodata');
+	}
+
+    public function penjumlahan($nilai1, $nilai2)
+	{
+		echo $this->Model_aritmatika->penjumlahan($nilai1,$nilai2);
+	}
+}
---
+++ application/views/biodata.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Biodata Sederhana</title>
+	<style>
+		body {
+			background-color: #f7f7f7;
+			font-family: Arial, sans-serif;
+			color: #333;
+		}
+
+		h1 {
+			text-align: center;
+			color: #666;
+		}
+
+		.container {
+			max-width: 600px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+			border-radius: 5px;
+			text-align: center;
+		}
+
+
+		p {
+			margin-bottom: 10px;
+		}
+
+		.label {
+			font-weight: bold;
+			margin-right: 5px;
+		}
+
+		.data {
+			font-weight: normal;
+			display: inline-block;
+			margin-right: 20px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>Biodata Sederhana</h1>
+		<p><span class="label">Nama:</span><span class="data">Adham Zafi</span></p>
+		<p><span class="label">Usia:</span><span class="data">20 tahun</span></p>
+		<p><span class="label">Alamat:</span><span class="data">Jl. KP Banjir Kanal, Jakarta</span></p>
+		<p><span class="label">Pendidikan:</span><span class="data">S1 Teknologi Informasi</span></p>
+		<p><span class="label">Email:</span><span class="data"><a href="adhamzafi91@gmail.com">adhamzafi91@gmail.com</a></span></p>
+		<p><span class="label">Telepon:</span><span class="data"><a href="tel:+62895320072311">0895320072311</a></span></p>
+	</div>
+</body>
+</html>
