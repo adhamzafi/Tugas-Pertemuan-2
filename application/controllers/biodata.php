f3a6e7ffe173480550dc730292a0f937797db426
Adham Zafi <adhamzafi91@gmail.com>
2023-04-06 17:51:09 +0700

    Tugas Pertemuan 3

--- application/controllers/biodata.php
+++ application/controllers/biodata.php
@@ -10,6 +10,9 @@ class biodata extends CI_Controller {
 
     public function penjumlahan($nilai1, $nilai2)
 	{
-		echo $this->Model_aritmatika->penjumlahan($nilai1,$nilai2);
+		$data['nilai1'] = $nilai1;
+		$data['nilai2'] = $nilai2;
+		$data['hasil'] = $this->Model_aritmatika->penjumlahan($nilai1, $nilai2);
+		$this->load->view('view-latihan.php', $data);
 	}
 }
---
+++ application/views/view-latihan.php
@@ -0,0 +1,12 @@
+<head>
+    <title>Latihan penjumlahan</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu
+    <?= $nilai1; ?> + <?= $nilai2; ?> = <?= $hasil; ?>
+    
+</body>
