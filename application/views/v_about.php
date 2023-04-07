178cb20ecdea13bef1a33afea3c2376acb2579ec
Adham Zafi <adhamzafi91@gmail.com>
2023-04-07 10:52:11 +0700

    Revisi Tugas Pertemuan 3

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,22 +2,22 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan : Adham </li>
+        <li>Nama Belakang : Zafi</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+        <li> Jalan KP Banjir Kanal no.14 Grogol</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Cirebon</li>
+        <li>Jakarta</li>
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>Bulutangkis</li>
-        <li>Catur</li>
+        <li>Sepak Bola</li>
+        <li>Futsal</li>
     </ul>
 </section>
